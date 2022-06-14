3a7fa4fbee90ce523a3b7a2a7d5f599aba8dd9be
connorwfitch <connorwfitch@gmail.com>
2022-06-14 16:40:03 -0400

    form submit works

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,8 @@
 import psycopg2
 import os
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect
+from .forms import AppointmentForm
+from datetime import datetime
 
 
 bp = Blueprint("main", __name__, url_prefix="/")
@@ -12,10 +14,27 @@ CONNECTION_PARAMETERS = {
     "host": os.environ.get("DB_HOST"),
 }
 
-@bp.route("/")
+@bp.route("/", methods=["GET", "POST"])
 def main():
+  form = AppointmentForm()
+
+  if form.validate_on_submit():
+    with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
+      with conn.cursor() as curs:
+        params = {
+          'name': form.name.data,
+          'start_datetime': datetime.combine(form.start_date.data, form.start_time.data),
+          'end_datetime': datetime.combine(form.end_date.data, form.end_time.data),
+          'description': form.description.data,
+          'private': form.private.data
+        }
+        curs.execute(f"""INSERT INTO appointments 
+        (name, start_datetime, end_datetime, description, private)
+        VALUES (%s, %s, %s, %s, %s)
+        """, (params['name'], params['start_datetime'], params['end_datetime'], params['description'], params['private']))
+    return redirect('/')
   with psycopg2.connect(**CONNECTION_PARAMETERS) as conn:
     with conn.cursor() as curs:
       curs.execute("SELECT id, name, start_datetime, end_datetime FROM appointments ORDER BY start_datetime;")
       rows = curs.fetchall()
-      return render_template("main.html", rows=rows)
+      return render_template("main.html", rows=rows, form=form)

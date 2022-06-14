b78eac5723f88fe83647af77871b9b8fd65695be
connorwfitch <connorwfitch@gmail.com>
2022-06-14 16:47:28 -0400

    light form validations

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,8 @@
+import imp
 from flask_wtf import FlaskForm
 from wtforms.fields import BooleanField, DateField, StringField, SubmitField, TextAreaField, TimeField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, ValidationError
+from datetime import datetime
 
 class AppointmentForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
@@ -11,3 +13,13 @@ class AppointmentForm(FlaskForm):
     description = TextAreaField("Description", validators=[DataRequired()])
     private = BooleanField("Private")
     submit = SubmitField("Submit")
+
+    def validate_end_date(form, field):
+        start = datetime.combine(form.start_date.data, form.start_time.data)
+        end = datetime.combine(field.data, form.end_time.data)
+        if start >= end:
+            msg = "End date/time must come after start date/time"
+            raise ValidationError(msg)
+        if start.day != end.day:
+            msg ="End date must be same as start date"
+            raise ValidationError(msg)
